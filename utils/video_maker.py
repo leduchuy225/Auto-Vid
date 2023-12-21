13fc8c4bd7ccca687b9883577b3ae6e8dba35167
Huy LĐ <leduchuy2205@gmail.com>
2023-12-21 22:57:26 +0700

    test subtitle

--- utils/video_maker.py
+++ utils/video_maker.py
@@ -1,4 +1,5 @@
 import moviepy.editor as mp
+from moviepy.video.tools.subtitles import SubtitlesClip
 
 
 def generateVideo(audio, images, videoName='video.mp4'):
@@ -14,5 +15,18 @@ def generateVideo(audio, images, videoName='video.mp4'):
 
   # Set the audio of the clip
   clip = clip.set_audio(audioFile)
+
+  # def generator(txt): return mp.TextClip(
+  #     txt, font='Arial', fontsize=24, color='white')
+
+  # subs = [((0, 4), 'subs1'),
+  #         ((4, 9), 'subs2'),
+  #         ((9, 12), 'subs3'),
+  #         ((12, 16), 'subs4')]
+
+  # subtitles = SubtitlesClip(subs).set_pos(('center', 'bottom'))
+
+  # clip = mp.CompositeVideoClip([clip, subtitles])
+
   # Export the clip
   clip.write_videofile(videoName, fps=24)
